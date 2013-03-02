f1c0c7b923d3bf9ca052874e11f74e749a428499
Harriet Compton-Moen <harrietcomptonmoen@selhouse.school.nz>
2013-03-02 15:27:32 +1300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guesstocat::Application.config.secret_token = '96f255fdccbda769eb6975f3390de4f9d4a0efbe06d5220c289e861f55fd980c030dd7d6020904ac7fcca2706a49f7595c3e5c9889653ce05904af201e44dbd0'
